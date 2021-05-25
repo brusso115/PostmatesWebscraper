f6063c27d41a0e2418360416a3a006368012a11e
brusso115 <brusso115@yahoo.com>
2021-05-24 23:21:57 -0400

    Removing stopwords and lemmatizing bow, adding distribution of menu item prices to dashboard

--- postmatesDashboard.py
+++ postmatesDashboard.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import nltk
 import numpy as np
 import plotly
 import plotly.express as px  # (version 4.7.0)
@@ -10,8 +11,15 @@ import dash_table
 from dash.dependencies import Input, Output
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+
+nltk.download('stopwords')
+nltk.download('wordnet')
 import plotly.io as plt_io
 from matplotlib.pyplot import cm
+wn.ensure_loaded()
 
 app = dash.Dash(__name__)
 
@@ -23,8 +31,9 @@ postmates = postmates.loc[:, ~postmates.columns.str.contains('^Unnamed')]
 
 postmatesBOW = pd.read_csv("postmates_BOW.csv")
 #postmatesBOW = postmates.loc[:, ~postmatesBOW.columns.str.contains('^Unnamed')]
-print(postmatesBOW.columns)
+#print(postmatesBOW.columns)
 
+postmatesPopItems = pd.read_csv('postmatesPopItems.csv')
 
 # Add scale column to scale favorites column
 postmates_grouped = postmates.groupby(['Name', 'Category', 'Latitude', 'Longitude']).mean().reset_index()
@@ -34,8 +43,8 @@ postmates_grouped["scale"] = (postmates_grouped["Favorites"] - postmates_grouped
 
 #Train model
 count = CountVectorizer()
-countVec = count.fit(postmatesBOW['BagOfWords'])
-count_matrix = countVec.transform(postmatesBOW['BagOfWords'])
+countVec = count.fit(postmatesBOW['BagOfWords'].values.astype('U'))
+count_matrix = countVec.transform(postmatesBOW['BagOfWords'].values.astype('U'))
 
 #Plot map figure
 fig = px.scatter_mapbox(postmates_grouped, lat="Latitude", lon="Longitude",
@@ -69,7 +78,6 @@ app.layout = html.Div([
         value='',
         style={'height': '40px','backgroundColor':'rgb(51, 52, 50)','border': 'rgb(17,17,17)',
                'border-radius': '5px', 'width':'99%','color':'white','padding-left':'10px'},
-        #return fig
     ),
 
     html.Br(),
@@ -78,7 +86,6 @@ app.layout = html.Div([
     dash_table.DataTable(
         id='table',
         columns=[{"name": i, "id": i} for i in postmates.columns[[0, 4, 5, 6]]],
-        #data=postmates.to_dict('records'),
         style_cell={
             'whiteSpace': 'normal',
             'height': 'auto',
@@ -105,38 +112,32 @@ app.layout = html.Div([
         ],
 
         style_as_list_view=True
-    )
+    ),
+
+    html.H1("Price Analysis of 1000 Most Frequently Occuring Menu Items", style={'text-align': 'center', 'backgroundColor':'rgb(17,17,17)', 'color': 'white'}),
+
+    dcc.Dropdown(id="menu_item",
+                 options=[{'label': i, 'value': i} for i in postmatesPopItems['MenuItem'].unique()],
+                 value='French Fries',
+                 style={'height': '40px', 'backgroundColor': 'rgb(51, 52, 50)', 'border': 'rgb(17,17,17)',
+                        'border-radius': '5px', 'width': '99%', 'color': 'white', 'fontColor' : 'white',
+                        'textColor' : 'white'
+                        },
+                 ),
+
+    html.Br(),
+    html.Br(),
+
+    dcc.Graph(id="distplot"),
+
+    html.Br(),
+
+    dcc.Graph(id='barchart')
 
 ])
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
-'''
-@app.callback(
-    Output(component_id='postmates_map', component_property='figure')
-    #[Input(component_id='slct_category', component_property='value')]
-)
-def update_graph():
-    
-    if option_slctd == []:
-        postmatesGrouped = postmates_grouped[postmates_grouped['Category'].isin(list(postmates_grouped['Category']))]
-    else:
-        postmatesGrouped = postmates_grouped[postmates_grouped['Category'].isin(option_slctd)]
-    
-
-    postmatesGrouped = postmates_grouped.copy()
-
-    fig = px.scatter_mapbox(postmatesGrouped, lat="Latitude", lon="Longitude",
-                            size="scale", hover_name="Name", hover_data=["Favorites"], color="Category", zoom=10,
-                            height=800, template="plotly_dark",
-                            )
-    fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
-    fig.update_traces(marker={'sizemin': 4})
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    fig.update_layout(uirevision=True)
-
-    return fig
-'''
 
 @app.callback(
     Output(component_id='table', component_property='data'),
@@ -145,8 +146,14 @@ def update_graph():
 def update_table(meal_description):
 
     postmatesBOWCopy = postmatesBOW.copy()
+    lemmatizer = WordNetLemmatizer()
 
-    user_input = [meal_description]
+    user_input = meal_description.split(' ')
+    user_input = [word for word in user_input if word not in stopwords.words('english')]
+    user_input = [lemmatizer.lemmatize(word) for word in user_input]
+    user_input = ' '.join(user_input)
+    user_input = [user_input]
+
     count_matrix_inp = countVec.transform(user_input)
 
     cosine_sim = cosine_similarity(count_matrix, count_matrix_inp)
@@ -156,6 +163,61 @@ def update_table(meal_description):
 
     return postmatesBOWCopy.to_dict('records')
 
+@app.callback(
+    Output(component_id='distplot', component_property='figure'),
+    [Input(component_id='menu_item', component_property='value')]
+)
+def update_distplot(menu_item):
+
+    postmatesPopItemsCopy = postmatesPopItems.copy()
+    postmatesPopItemsCopy = postmatesPopItemsCopy[postmatesPopItemsCopy['MenuItem'] == menu_item]
+    postmatesPopItemsCopy.drop_duplicates(subset=['Name'], keep='first', inplace=True)
+    postmatesPopItemsCopy = postmatesPopItemsCopy[pd.notnull(postmatesPopItemsCopy['MenuItemPrice'])]
+
+    dp = px.histogram(
+        postmatesPopItemsCopy,
+        x='MenuItemPrice',
+        hover_data=postmatesPopItemsCopy.columns,
+        template='plotly_dark',
+        nbins=30,
+        title='Distribution of Menu Item Price',
+        labels={
+            "MenuItemPrice": "Menu Item Price",
+            "count": "Count",
+        },
+    )
+    return dp
+
+@app.callback(
+    Output(component_id='barchart', component_property='figure'),
+    [Input(component_id='menu_item', component_property='value')]
+)
+def update_barchart(menu_item):
+
+    postmatesPopItemsCopy = postmatesPopItems.copy()
+    postmatesPopItemsCopy = postmatesPopItemsCopy[postmatesPopItemsCopy['MenuItem'] == menu_item]
+    postmatesPopItemsCopy = postmatesPopItemsCopy.sort_values('MenuItemPrice')
+    postmatesPopItemsCopy.drop_duplicates(subset=['Name'], keep='first', inplace=True)
+    postmatesPopItemsCopy = postmatesPopItemsCopy[pd.notnull(postmatesPopItemsCopy['MenuItemPrice'])]
+
+    print(postmatesPopItemsCopy.sort_values('MenuItemPrice',ascending=False)[['Name','MenuItemPrice']])
+
+    bc = px.bar(
+        postmatesPopItemsCopy,
+        x='Name', y='MenuItemPrice',
+        template='plotly_dark',
+        title='Price That Food Establishment Sells Item',
+        labels={
+            "Name": "Food Establishment Name",
+            "MenuItemPrice": "Menu Item Price",
+        },
+    )
+
+    bc.update_xaxes(
+        tickangle=45,
+    )
+
+    return bc
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     app.run_server(debug=True)
